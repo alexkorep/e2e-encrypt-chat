137a108dedb5567e62d1457a437a6681335c1809
Alexey Korepanov <alexkorep@gmail.com>
2020-02-02 20:09:36 +0100

    Add react-web-gifted-chat

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import PubNub from 'pubnub';
+import { GiftedChat } from 'react-web-gifted-chat';
+
 import './App.css';
 
 const pubnub = new PubNub({
-  publishKey: 'YOUR_PUBLISH_KEY_HERE',
-  subscribeKey: 'YOUR_SUBSCRIBE_KEY_HERE',
+  publishKey: 'pub-c-5e44bacc-d9b4-4d11-9462-089626f7f2e6',
+  subscribeKey: 'sub-c-347badce-45ed-11ea-aea8-722f8d3d4603',
 });
 // var box = document.getElementById("box"), input = document.getElementById("input"), channel = 'chat';
 
@@ -15,16 +17,35 @@ const pubnub = new PubNub({
 // });
 
 const channel = 'chat';
+const userId = Math.random().toString(10).slice(2);
+console.log(userId)
 
 function App() {
-  const [message, setMessage] = useState('');
-  const [history, setHistory] = useState([]);
+  const [messages, setMessages] = useState([
+    // {
+    //   id: 1,
+    //   text: 'Hello developer',
+    //   createdAt: new Date(),
+    //   user: {
+    //     id: 2,
+    //     name: 'React',
+    //     avatar: 'https://facebook.github.io/react/img/logo_og.png',
+    //   },
+    // },
+  ]);
 
   useEffect(() => {
     const listener = {
       message: function (m) {
         // box.innerHTML = ('' + m.message).replace(/[<>]/g, '') + '<br>' + box.innerHTML; // Add message to page.
         console.log(m);
+        const { message } = m;
+        const { messages } = message;
+        const newMessages = messages.filter(msg => {
+          return msg.user.id !== userId
+        })
+        setMessages((oldMessages) =>
+          (GiftedChat.append(oldMessages, newMessages)));
       },
     };
     pubnub.subscribe({ channels: [channel] }); // Subscribe to a channel.
@@ -35,18 +56,65 @@ function App() {
     };
   }, []);
 
+  const sendMessage = (newMessages) => {
+    setMessages((oldMessages) =>
+      (GiftedChat.append(oldMessages, newMessages)));
+    pubnub.publish({
+      channel: channel,
+      message: {
+        messages: newMessages
+      }
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          <input type="text" value={message} onChange={(e) => {
-            setMessage(e.target.value);
-          }} />
-          <button>Send</button>
-        </p>
-      </header>
+    <div className="App" style={styles.container}>
+      <div style={styles.conversationList}>
+        Converstions
+        </div>
+      <div style={styles.chat}>
+        <GiftedChat
+          messages={messages}
+          onSend={(newMessages) => sendMessage(newMessages)}
+          user={{
+            id: userId,
+            name: userId.toString(),
+            avatar: `https://i.pravatar.cc/36?u=${userId}`,
+          }}
+        />
+      </div>
+      <div style={styles.converationDetails}>
+        Conversation details
+        </div>
     </div>
-  );
+  )
+};
+
+
+const styles = {
+  container: {
+    flex: 1,
+    display: "flex",
+    flexDirection: "row",
+    height: "100vh",
+  },
+  conversationList: {
+    display: 'flex',
+    flex: 1,
+  },
+  chat: {
+    display: "flex",
+    flex: 3,
+    flexDirection: "column",
+    borderWidth: "1px",
+    borderColor: "#ccc",
+    borderRightStyle: "solid",
+    borderLeftStyle: "solid",
+  },
+  converationDetails: {
+    display: 'flex',
+    flex: 1,
+  }
 }
 
 export default App;
