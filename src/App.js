10a5665b2a0ab02690ffbbb16302850bb9045a89
Alexey Korepanov <alexkorep@gmail.com>
2020-02-02 11:15:42 +0100

    Add PubNub library

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import PubNub from 'pubnub';
 import './App.css';
 
+const pubnub = new PubNub({
+  publishKey: 'YOUR_PUBLISH_KEY_HERE',
+  subscribeKey: 'YOUR_SUBSCRIBE_KEY_HERE',
+});
+// var box = document.getElementById("box"), input = document.getElementById("input"), channel = 'chat';
+
+// input.addEventListener('keypress', function (e) {
+//   (e.keyCode || e.charCode) === 13 && pubnub.publish({ // Publish new message when enter is pressed.
+//     channel: channel, message: input.value, x: (input.value = '')
+//   });
+// });
+
+const channel = 'chat';
+
 function App() {
+  const [message, setMessage] = useState('');
+  const [history, setHistory] = useState([]);
+
+  useEffect(() => {
+    const listener = {
+      message: function (m) {
+        // box.innerHTML = ('' + m.message).replace(/[<>]/g, '') + '<br>' + box.innerHTML; // Add message to page.
+        console.log(m);
+      },
+    };
+    pubnub.subscribe({ channels: [channel] }); // Subscribe to a channel.
+    pubnub.addListener(listener);
+    return () => {
+      pubnub.unsubscribe({ channels: [channel] });
+      pubnub.removeListener(listener);
+    };
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <input type="text" value={message} onChange={(e) => {
+            setMessage(e.target.value);
+          }} />
+          <button>Send</button>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
